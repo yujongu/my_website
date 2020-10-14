2ff701125cd48c2fda9cb3a21a92676d9e566a9c
Jongu Yu <yujongu@gmail.com>
2020-10-14 17:10:10 +0900

    Initial Commit!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Unsplash, { toJson } from "unsplash-js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./routes/Home";
+import "./App.css";
+
+global.fetch = fetch;
+const accKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+const secKey = process.env.REACT_APP_UNSPLASH_SECRET_KEY;
+
+const unsplash = new Unsplash({
+  accessKey: accKey,
+  secret: secKey,
+});
+
+class App extends React.Component {
+  fetchImage = () => {
+    unsplash.photos
+      .getRandomPhoto({
+        orientation: "landscape",
+      })
+      .then(toJson)
+      .then((json) => {
+        console.log(json.urls);
+        var main = document.getElementById("main");
+        main.style.backgroundImage = `url(${json.urls.full})`;
+        main.style.backgroundSize = "100%, auto";
+      });
+  };
+
+  componentDidMount() {
+    this.fetchImage();
+  }
+
+  render() {
+    return (
+      <HashRouter>
+        <Route path="/" exact={true} component={Home} />c
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+function Home() {
+  return (
+    <div className="body">
+      <h1>Joey</h1>
+    </div>
+  );
+}
+
+export default Home;
