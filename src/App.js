b167e78d415014b44f9efb47960a259487387f06
Jongu Yu <yujongu@gmail.com>
2020-10-15 17:33:31 +0900

    Work on navigation and left nav animation

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ import "./App.css";
 global.fetch = fetch;
 
 class App extends React.Component {
-  setBackgroundImage = () => {
-    var a = Math.floor(Math.random() * 5);
-    var body = document.getElementById("main");
-    body.style.backgroundImage = `linear-gradient(
-      rgba(0, 0, 0, 0.5), 
-      rgba(0, 0, 0, 0.5)
-    ),url(/img/background_img/${a + 1}.jpg)`;
-    body.style.backgroundSize = "100%, 100%";
-  };
-  componentDidMount() {
-    this.setBackgroundImage();
-  }
+  componentDidMount() {}
 
   render() {
     return (
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,41 @@
 import React from "react";
 import "./Home.css";
+import "../components/nav.css";
 function Home() {
   return (
     <div className="body">
+      <div id="intro">
+        <span>This is Joey</span>
+      </div>
+      <div className="nav__container">
+        <div className="nav__container-left">
+          <div className="nav__left-top">
+            <span>Bio</span>
+          </div>
+          <div className="nav__left">
+            <span>Work Experiences</span>
+          </div>
+          <div className="nav__left-bottom">
+            <span>Skills</span>
+          </div>
+        </div>
+        <div className="nav__container-right">
+          <div className="nav__right-top">
+            <span>Projects</span>
+          </div>
+          <div className="nav__right">
+            <span>Related courses</span>
+          </div>
+          <div className="nav__right-bottom">
+            <span>Interests</span>
+          </div>
+        </div>
+      </div>
+
       <div className="circular_img">
         <img
           className="profile__image"
-          src="/img/profile_picture.jpeg"
+          src="/img/sample_profile_picture.jpeg"
           alt="Profile"
           title="Profile"
           draggable="false"
